6dad19937c45c2f617007d2f8dd13c10dc7c319a
Mehmet Yusuf Ertekin <yusuf.ertekin@education.gov.uk>
2019-05-10 03:00:11 +0100

    tmp

--- gamesight/settings/dev.py
+++ gamesight/settings/dev.py
@@ -12,3 +12,13 @@ MIDDLEWARE += [
 ]
 
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
