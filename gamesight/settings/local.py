a0935a6fc3d459ea529bcdb7addb12d9dbdddf1d
Mehmet Yusuf Ertekin <yusuf.ertekin@education.gov.uk>
2019-05-13 11:44:25 +0300

    basic home

--- gamesight/settings/local.py
+++ gamesight/settings/local.py
@@ -1,8 +1,33 @@
-from gamesight.settings.dev import *
+from gamesight.settings.base import *
+
+DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INSTALLED_APPS += [
+    'django_extensions',
+    'debug_toolbar', 
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
